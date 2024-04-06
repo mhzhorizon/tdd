47395d52c6c8c6a2931d83ce88443298fdeafd13
mhzhorizon <mohamed.hadi.zaira@horizon-tech.tn>
2024-04-06 12:42:38 +0200

    3c

--- AdditionCalculatorTest.java
+++ AdditionCalculatorTest.java
@@ -20,5 +20,16 @@ public class AdditionCalculatorTest {
 	        AdditionCalculator calculator = new AdditionCalculator();
 	        assertEquals(8, calculator.add(5, 3));
 	    }
+	 
+	 @Test
+	    void testNonIntegerInputs() {
+	        AdditionCalculator calculator = new AdditionCalculator();
+	        try {
+	            calculator.add(5, 1/3);
+	            fail("Expected IllegalArgumentException");
+	        } catch (IllegalArgumentException e) {
+	            assertEquals("Input is not an integer.", e.getMessage());
+	        }
+	    }
 
 }
